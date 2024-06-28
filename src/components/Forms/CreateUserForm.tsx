a658d33997e75863f8f06ef3134c94fd92a15db9
Prakash Pawar <pp886655@gmail.com>
2024-06-28 14:32:10 +0530

    default option Select Role

--- src/components/Forms/CreateUserForm.tsx
+++ src/components/Forms/CreateUserForm.tsx
@@ -297,6 +297,9 @@ const CreateUserForm = () => {
                   onChange={handleChange}
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 >
+                  <option value="">
+                    Select Role
+                  </option>
                   <option value="INTERVIEW_CANDIDATE">
                     Interview Candidate
                   </option>
